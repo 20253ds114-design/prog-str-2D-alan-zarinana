b964b0ec106b66cd19e2d8d3ae4dc20d55dbd5b9
20253ds114-design <20253ds114@utez.edu.mx>
2026-01-18 16:39:46 -0600

    tarea: codigo sin corregir

--- proyecto-01-hola-mundo/src/Main.java
+++
@@ -1,7 +0,0 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Alan Zariñana Arizmendi . 2D. 13/1/2026. Hola Mundo");
-    }
-}
