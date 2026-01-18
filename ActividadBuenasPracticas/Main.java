8f49dcd85a0cbfee4af391431b24dc8f77b093fb
20253ds114-design <20253ds114@utez.edu.mx>
2026-01-18 17:00:46 -0600

    tarea modificada: version clara del codigo

--- ActividadBuenasPracticas/Main.java
+++ ActividadBuenasPracticas/Main.java
@@ -1,13 +1,22 @@
 import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] a) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("n:");
-        int n = sc.nextInt();
-        int s = 0;
-        for (int i = 1; i <= n; i++) {
-            s += i;
+    public static void main(String[] args) {
+        int numeroLimite = pedirEntero();
+        int resultadoSuma = sumarHastaN(numeroLimite);
+        System.out.println("La suma de los números es: " + resultadoSuma);
+    }
+    public static int pedirEntero() {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("ingresa un número entero positivo: ");
+        return teclado.nextInt();
+    }
+
+    public static int sumarHastaN(int limite) {
+        int sumaAcumulada = 0;
+        for (int i = 1; i <= limite; i++) {
+            sumaAcumulada += i;
         }
-        System.out.println("r:" + s);
+        return sumaAcumulada;
     }
 }
