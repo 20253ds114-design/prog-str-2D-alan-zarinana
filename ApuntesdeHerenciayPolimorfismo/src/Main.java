0784165622958556ae41e666d91162f5da6646bc
20253ds114-design <20253ds114@utez.edu.mx>
2026-02-13 23:53:58 -0600

    apuntes de clase

---
+++ ApuntesdeHerenciayPolimorfismo/src/Main.java
@@ -0,0 +1,38 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Herencia
+        Perro dogo = new Perro("Dogo");
+        Gato rawrl = new Gato("Rawrl");
+
+        dogo.comer();
+        dogo.hacerSonido();
+        dogo.marcarTerritorio();
+
+        rawrl.comer();
+        rawrl.hacerSonido();
+
+        System.out.println("-------------");
+
+        // Polimorfismo
+        Animal[] animales = new Animal[3];
+        animales[0] = new Perro("Ramonchis");
+        animales[1] = new Gato("Salem");
+        animales[2] = new Perro("Firulais");
+
+        for (Animal animal : animales) {
+            animal.hacerSonido();
+        }
+
+        System.out.println("___________________");
+
+        Animal miercoles = new Perro("Miercoles");
+
+        if (miercoles instanceof Perro) {
+            Perro miercoles1 = (Perro) miercoles;
+            miercoles1.marcarTerritorio();
+        }
+    }
+}
+
